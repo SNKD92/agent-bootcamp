94dc1fb80ba80a04493233e8a9131754f3ef9f50
default <deniskachar@gmail.com>
2025-12-03 21:05:44 +0000

    added imports

---
+++ src/reliabot/app.py
@@ -0,0 +1,19 @@
+import asyncio
+import contextlib
+import logging
+import signal
+import sys
+
+import agents
+import gradio as gr
+from dotenv import load_dotenv
+from gradio.components.chatbot import ChatMessage
+from openai import AsyncOpenAI
+
+from src.prompts import REACT_INSTRUCTIONS
+from src.utils import (
+    AsyncWeaviateKnowledgeBase,
+    Configs,
+    get_weaviate_async_client,
+    oai_agent_stream_to_gradio_messages,
+)

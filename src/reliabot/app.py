0519fc167fe76b8a0e1eab1ce21085f7f878b23d
default <deniskachar@gmail.com>
2025-12-09 15:16:12 +0000

    our app

--- src/reliabot/app.py
+++ src/reliabot/app.py
@@ -1,19 +1,54 @@
-import asyncio
-import contextlib
-import logging
-import signal
-import sys
-
-import agents
 import gradio as gr
 from dotenv import load_dotenv
-from gradio.components.chatbot import ChatMessage
 from openai import AsyncOpenAI
 
-from src.prompts import REACT_INSTRUCTIONS
 from src.utils import (
     AsyncWeaviateKnowledgeBase,
     Configs,
     get_weaviate_async_client,
-    oai_agent_stream_to_gradio_messages,
+    pretty_print,
 )
+
+configs = Configs.from_env_var()
+async_weaviate_client = get_weaviate_async_client(
+    http_host=configs.weaviate_http_host,
+    http_port=configs.weaviate_http_port,
+    http_secure=configs.weaviate_http_secure,
+    grpc_host=configs.weaviate_grpc_host,
+    grpc_port=configs.weaviate_grpc_port,
+    grpc_secure=configs.weaviate_grpc_secure,
+    api_key=configs.weaviate_api_key,
+)
+async_openai_client = AsyncOpenAI()
+async_knowledgebase = AsyncWeaviateKnowledgeBase(
+    async_weaviate_client,
+    collection_name="enwiki_20250520",
+)
+
+DESCRIPTION = """\
+In the example below, we check our knowledge \
+
+"""
+
+async def search_and_pretty_format(keyword: str) -> str:
+    """Search knowledgebase and pretty-format output."""
+    output = await async_knowledgebase.search_knowledgebase(keyword)
+    return pretty_print(output)
+
+json_codeblock = gr.Code(language="json", wrap_lines=True)
+
+
+demo = gr.Interface(
+    fn=search_and_pretty_format,
+    inputs=["text"],
+    outputs=[json_codeblock],
+    title="1.0: Knowledge Base ADR demo",
+    description=DESCRIPTION,
+    examples=[
+        "Nananana",
+        "ButtCheek",
+        "Chris P. Bacon"
+    ],
+)
+
+demo.launch(share=True)

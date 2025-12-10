4da3d8fcd9a12b1f14e6795bffbf4fe177846e9c
default <deniskachar@gmail.com>
2025-12-10 19:36:10 +0000

    new version

--- src/reliabot/app.py
+++ src/reliabot/app.py
@@ -1,13 +1,37 @@
+"""Reliabot.
+
+Log traces to LangFuse for observability and evaluation.
+"""
+
+import asyncio
+import contextlib
+import signal
+import sys
+
+import agents
 import gradio as gr
 from dotenv import load_dotenv
+from gradio.components.chatbot import ChatMessage
 from openai import AsyncOpenAI
 
+from src.prompts import REACT_INSTRUCTIONS
 from src.utils import (
     AsyncWeaviateKnowledgeBase,
     Configs,
     get_weaviate_async_client,
+    oai_agent_stream_to_gradio_messages,
     pretty_print,
+    set_up_logging,
+    setup_langfuse_tracer,
 )
+from src.utils.langfuse.shared_client import langfuse_client
+
+
+load_dotenv(verbose=True)
+
+set_up_logging()
+
+AGENT_LLM_NAME = "gemini-2.5-flash"
 
 configs = Configs.from_env_var()
 async_weaviate_client = get_weaviate_async_client(
@@ -25,31 +49,115 @@ async_knowledgebase = AsyncWeaviateKnowledgeBase(
     collection_name="Devops_reasoning_traces",
 )
 
-DESCRIPTION = """\
-In the example below, we check our knowledge \
 
-"""
-
-async def search_and_pretty_format(keyword: str) -> str:
-    """Search knowledgebase and pretty-format output."""
-    output = await async_knowledgebase.search_knowledgebase(keyword)
-    return pretty_print(output)
+async def _cleanup_clients() -> None:
+    """Close async clients."""
+    await async_weaviate_client.close()
+    await async_openai_client.close()
 
 
-json_codeblock = gr.Code(language="json", wrap_lines=True)
+def _handle_sigint(signum: int, frame: object) -> None:
+    """Handle SIGINT signal to gracefully shutdown."""
+    with contextlib.suppress(Exception):
+        asyncio.get_event_loop().run_until_complete(_cleanup_clients())
+    sys.exit(0)
 
 
-demo = gr.Interface(
-    fn=search_and_pretty_format,
-    inputs=["text"],
-    outputs=[json_codeblock],
-    title="1.0: Knowledge Base ADR demo",
-    description=DESCRIPTION,
+async def _main(question: str, gr_messages: list[ChatMessage]):
+    setup_langfuse_tracer()
+
+   """Initialize MCP Git server and run the agent."""
+    
+    
+    repo_path = os.path.abspath("/home/coder/agent-bootcamp")
+
+
+        async with MCPServerStdio(
+            name="Git server",
+            params={
+                "command": "uvx",
+                "args": ["mcp-server-git"],
+            },
+            tool_filter=create_static_tool_filter(
+                allowed_tool_names=["git_status", "git_log"]
+            ),
+        ) as mcp_server:
+            agent = agents.Agent(
+                name="Git Assistant",
+                instructions=f"Answer questions about the git repository at {repo_path}, use that for repo_path",
+                mcp_servers=[mcp_server],
+                model=agents.OpenAIChatCompletionsModel(
+                    model=AGENT_LLM_NAME, openai_client=async_openai_client
+                ),
+            )
+    reasoning_agent = agents.Agent(.
+        name="Reliabot",
+        instructions=REACT_INSTRUCTIONS,
+        tools=[agents.function_tool(async_knowledgebase.search_knowledgebase)],
+        model=agents.OpenAIChatCompletionsModel(
+            model=AGENT_LLM_NAME, openai_client=async_openai_client
+        ),
+    )
+
+    main_agent = agents.Agent(
+        name="MainAgent",
+        instructions="""
+        """,
+        # Allow the planner agent to invoke the worker agent.
+        # The long context provided to the worker agent is hidden from the main agent.
+        tools=[
+            reasoning_agent.as_tool(
+                tool_name="reasoning_agent",
+                tool_description=(
+                    "Search the knowledge base for a query and return a concise summary "
+                    "of the key findings, along with the sources used to generate "
+                    "the summary"
+                ),
+            ),
+            agent.as_tool(
+                tool_name="Git MCP server",
+                tool_description=(
+                    "Search the Git repo for a info about the issue "
+                ),  
+            ),
+        ],
+        # a larger, more capable model for planning and reasoning over summaries
+        model=agents.OpenAIChatCompletionsModel(
+            model=AGENT_LLM_NAMES, openai_client=async_openai_client
+        ),
+    )
+    with langfuse_client.start_as_current_span(name="Reliabot") as span:
+        span.update(input=question)
+
+        result_stream = agents.Runner.run_streamed(main_agent, input=question)
+        async for _item in result_stream.stream_events():
+            gr_messages += oai_agent_stream_to_gradio_messages(_item)
+            if len(gr_messages) > 0:
+                yield gr_messages
+
+        span.update(output=result_stream.final_output)
+
+    pretty_print(gr_messages)
+    yield gr_messages
+
+
+demo = gr.ChatInterface(
+    _main,
+    title="0.1beta Reliabot + LangFuse",
+    type="messages",
     examples=[
-        "Nananana",
-        "ButtCheek",
-        "Chris P. Bacon"
+        "Where in CI/CD pipeline the job stuck when GitLab runner lose connection during deploy?"
+        "Which microservice caused latency spike when Istio sidecar injected wrongly configured certificates?",
     ],
 )
 
-demo.launch(share=True)
+
+if __name__ == "__main__":
+    configs = Configs.from_env_var()
+
+    signal.signal(signal.SIGINT, _handle_sigint)
+
+    try:
+        demo.launch(share=True)
+    finally:
+        asyncio.run(_cleanup_clients())
